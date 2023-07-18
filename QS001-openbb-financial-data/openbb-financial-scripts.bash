9babce70474d781e817e4210e677763d9704e6e0
Matt Dancho <mdancho@gmail.com>
2023-07-18 09:36:33 -0400

    QS001: openbb scripts

---
+++ QS001-openbb-financial-data/openbb-financial-scripts.bash
@@ -0,0 +1,33 @@
+# The Quant Science Newsletter
+# Edition 1: Get a $29,000 Financial Data Terminal for Free
+# Copyright: Quant Science, LLC
+
+# STEP 1: DOWNLOAD OPENBB
+# Download the terminal at: https://my.openbb.co/app/terminal/download
+
+# STEP 2: ANALYZE STOCKS
+stocks 
+
+# STEP 3: LOAD A STOCK
+load tsla 
+
+# STEP 4: GETTING HELP
+help 
+
+# STEP 5: PERFORM TECHNICAL ANALYSIS
+ta 
+
+# STEP 6: MAKE A TECHNICAL CHART
+sma 
+
+# STEP 7: BACK OUT OF TECHNICAL ANALYSIS & GET STOCKS SUBMENU
+q 
+help 
+
+# STEP 8: TIME SERIES (EXPERIMENTAL)
+forecast 
+autoarima -d TSLA --target-column adj_close -n 14
+
+# NEXT STEPS:
+# Sign up for our Python for Algorithmic Trading Course Waitlist
+#  https://learn.quantscience.io/python-algorithmic-trading-course-waitlist
