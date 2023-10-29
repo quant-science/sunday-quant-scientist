0358371bea7395e80e9b419fe3b7a684b307bb02
Matt Dancho <mdancho@gmail.com>
2023-10-29 12:26:53 -0400

    code updates

--- QS006-anomaly-buy-sell/01_anomaly_buy_sell.py
+++ QS006-anomaly-buy-sell/01_anomaly_buy_sell.py
@@ -17,10 +17,18 @@ stocks_df['date'] = pd.to_datetime(stocks_df['date'])
 
 stocks_df.glimpse()
 
-
-stocks_filtered_df = stocks_df.query('date >= "2021-01-01"')
+stocks_df.groupby('symbol').plot_timeseries(
+    date_column = 'date',
+    value_column = 'adjusted',
+    facet_ncol = 2,
+    width = 1100,
+    height = 800,
+    title = 'Stock Prices'
+)
 
 # 3.0 ANOMALY DETECTION: IDENTIFY BUY/SELL REGIONS ----
+
+stocks_filtered_df = stocks_df.query('date >= "2021-01-01"')
 
 stocks_anomalized_df = stocks_filtered_df[['symbol', 'date', 'adjusted']] \
     .groupby('symbol') \
